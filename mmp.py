529df6fb534d35f18ca3630b190513c1167c2a51
uenoshnj <aiueno6@gmail.com>
2024-10-04 17:53:09 +0900

    mmp.py, server.pyを修正(途中

--- mmp.py
+++ mmp.py
@@ -1,40 +1,45 @@
 import json
 
 # 受信用
-def _get_header(data: bytes):
+def _get_header(data: bytes) -> bytes:
     return data[:8]
 
-def _get_body(data: bytes):
+def _get_body(data: bytes) -> bytes:
     return data[8:]
 
-def get_json_size(data: bytes):
+def get_json_size(data: bytes) -> int:
     header: bytes = _get_header(data)
     return int.from_bytes(header[:2], 'big')
 
-def get_mediatype_size(data: bytes):
+def get_mediatype_size(data: bytes) -> int:
     header: bytes = _get_header(data)
     return int.from_bytes(header[2:3], 'big')
 
-def get_payload_size(data: bytes):
+def get_payload_size(data: bytes) -> int:
     header: bytes = _get_header(data)
     return int.from_bytes(header[3:8], 'big')
 
-
-def get_json(data: bytes):
+def get_json(data: bytes) -> dict:
     body: bytes = _get_body(data)
     json_size: int = get_json_size(data)
     return json.loads(body[:json_size].decode())
 
-def get_mediatype(data: bytes):
+def get_mediatype(data: bytes) -> bytes:
     body: bytes = _get_body(data)
     json_size: int = get_json_size(data)
     mediatype_size: int = get_mediatype_size(data)
     return body[json_size:json_size+mediatype_size].decode()
 
-def get_payload(data: bytes):
+def get_payload(data: bytes) -> bytes:
     body: bytes = _get_body(data)
     json_size: int = get_json_size(data)
     mediatype_size: int = get_mediatype_size(data)
     return body[json_size+mediatype_size:]
 
 # 送信用
+def create_packet(json_data: json, mediatype: str, payload: bytes) -> bytes:
+    json_size = len(json_data.encode('utf-8')).to_bytes(2, 'big')
+    mediatype_size = len(mediatype.encode('utf-8')).to_bytes(1, 'big')
+    payload_size = len(payload).to_bytes(5, 'big')
+
+    return json_size + mediatype_size + payload_size + json_data.encode('utf-8') + mediatype.encode('utf-8') + payload
--- server.py
+++ server.py
@@ -1,3 +1,4 @@
+import json
 import socket
 
 import mmp
@@ -8,7 +9,8 @@ class TcpServer:
         self.server_host: str = ''
         self.server_port: int = 9001
         
-        self.FILE_PATH: str = './tgt/received_video.mp4'
+        self.json_obj: dict = {}
+        self.tmp_file: str = './tmp/tmp_file'
 
         self.header: int = 8
         self.buffer: int = 1460
@@ -31,35 +33,62 @@ class TcpServer:
         conn, addr = self.sock.accept()
         print(f'Connected from {addr}')
         
+        # ヘッダー受信
         header: bytes = conn.recv(self.header)
         file_size: int = mmp.get_payload_size(header)
+        json_size: int = mmp.get_json_size(header)
+        media_type_size: int = mmp.get_mediatype_size(header)
 
         # ファイルバイト数が4TBを超えている場合
         if file_size > self.MAX_FILE_SIZE:
-            conn.sendall(self.RECEIVE_FILE_SIZE_EXCEED.to_bytes(self.header, byteorder='big'))
+            error_obj = ErrorHandle(self.RECEIVE_FILE_SIZE_EXCEED, 'File size exceeds 4TB', 'Reduce file size')
+            conn.sendall(mmp.create_packet(error_obj, '', b''))
             conn.close()
             return 1
         
-        data: bytes = b''
+        # jsonの受信
+        self.json_obj = json.load(conn.recv(json_size).decode('utf-8'))
+
+        # 一時保存ファイル名を作成
+        self.tmp_file += conn.recv(media_type_size).decode('utf-8')
+
         # ビデオファイル受信
-        while file_size > 0:
-            data += conn.recv(self.buffer)
-            file_size -= self.buffer
-        
-        return data
+        with open(self.tmp_file, 'wb') as f:
+            while file_size > 0:
+                data: bytes = conn.recv(self.buffer)
+                file_size -= self.buffer
+                f.write(data)
 
     # 通信
     def communication(self):
         
         # レスポンスの送信
         conn.sendall('Video file uploaded'.encode())
+
+
+class ErrorHandle:
+    def __init__(self, error_code: int, message: str, solusion: str):
+        self.error_obj: dict = {
+            'code' : error_code,
+            'message' : message,
+            'solusion': solusion
+        }
+    
     
 
 
-class VideoFileServer:
-    def __init__(self, data: bytes):
-        self.TMP_FILE_PATH: str = './tmp/received_video.mp4'
+class VideoProcessor:
+    def __init__(self, data: bytes, json_obj: dict):
+        self.TMP_FILE_PATH: str = './tmp/'
         self.recv_data: bytes = data
+        self.json_obj: dict = json_obj
+        self.functions: dict[str, function] = {
+            'compress': self.compress,
+            'chenge_resolution': self.chenge_resolution,
+            'change_aspect_ratio': self.change_aspect_ratio,
+            'convert_to_audio': self.convert_to_audio,
+            'create_gif_webm': self.create_gif_webm
+        }   
     
     # 一時領域に保存
     def save_tmp(self):
@@ -68,25 +97,30 @@ class VideoFileServer:
 
 
     # 動画圧縮
-    def video_compression(self):
+    def compress(self):
+        
         pass
 
     # 解像度の変更
-    def chenge_video_resolution(self):
+    def chenge_resolution(self):
         pass
 
     # アスペクト比の変更
-    def change_video_aspect_ratio(self):
+    def change_aspect_ratio(self):
         pass
 
     # 音声へ変換
-    def convert_video_to_audio(self):
+    def convert_to_audio(self):
         pass
 
     # GIF、WEBMを作成
     def create_gif_webm(self):
         pass
     
+    # 処理判断
+    def process_Handle(self):
+        self.json_obj
+        
 
 
 
